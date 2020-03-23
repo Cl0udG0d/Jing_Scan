dfbe85a7301430f74c5c9187087f55232d1d5750
潘一 <2585614464@qq.com>
2020-03-23 15:51:00 +0800

    添加命令行输入参数，完善A,B段扫描功能，添加requirements.txt文件

--- Scan.py
+++ Scan.py
@@ -2,6 +2,9 @@ import re
 import requests
 from fake_useragent import UserAgent
 from lxml import etree
+import sys
+import getopt
+ip=''
 port_list=[80, 81, 88, 808, 888, 8000,8008, 8080,8001, 8888, 8020,8009,8081,8082,8083]
 addr_url='http://ip.yqie.com/ip.aspx?ip='
 ua = UserAgent()
@@ -10,13 +13,33 @@ def headers_pool():
     headers = {"User-Agent": ua.random}
     return headers
 
-# def A_scan(ip):
-# def B_scan(ip):
+def A_scan(ip):
+    ip = ip.split('.')
+    host = []
+    for tmpAip in range(1, 256):
+        ip[1]=str(tmpAip)
+        for tmpBip in range(1, 256):
+            ip[-2] = str(tmpBip)
+            for tmpCip in range(1, 256):
+                ip[-1] = str(tmpCip)
+                host.append(".".join(ip))
+    return host
+
+def B_scan(ip):
+    ip=ip.split('.')
+    host=[]
+    for tmpBip in range(1,256):
+        ip[-2]=str(tmpBip)
+        for tmpCip in range(1,256):
+            ip[-1]=str(tmpCip)
+            host.append(".".join(ip))
+    return host
+
 def C_scan(ip):
     ip = ip.split('.')
     host = []
-    for tmpIP in range(1, 256):
-        ip[-1] = str(tmpIP)
+    for tmpCip in range(1, 256):
+        ip[-1] = str(tmpCip)
         host.append(".".join(ip))
     return host
 
@@ -55,22 +78,50 @@ def check_ip(ip):
     return False
 
 def scan(ip):
-    if(check_ip(ip)):
-        host=C_scan(ip)
-        for url in host:
-            for port in port_list:
-                get_rep(url,port)
+    host=C_scan(ip)
+    for url in host:
+        for port in port_list:
+            get_rep(url,port)
+
+
+def logo():
+    print "hi"
+
+def main(argv):
+    if len(sys.argv) <= 1:
+        exit("[-]please input ip,you can -h or --help\n")
+    try:
+        opts, args=getopt.getopt(argv[1:],"hi:t:m:",["help","ip=","thread=","method="])
+    except getopt.GetoptError:
+        exit("[-]please check your input true argv\n")
+    for opt,arg in opts:
+        if opt in ['-h','--help']:
+            print '''
+         [+]this is Jing_scan,To scan internet A,B or C range
+            you can input argvs:-h --help,-i --ip,-t --thread,-m --method
+            (Default scan C)
+            for example:
+            python2 Scan.py -i 127.0.0.1 -t 10 -m C
+            '''
+            sys.exit()
+        elif opt in ['-i','--ip']:
+            get_ip=arg
+        elif opt in ['-t','--thread']:
+            thread=arg
+        elif opt in ['-m','--method']:
+            method=arg
+        else:
+            exit("Error: invalid parameters :(")
+    logo()
+    if check_ip(get_ip):
+        if method=="A":
+            A_scan(ip)
+        elif method=="B":
+           B_scan(ip)
+        else:
+            C_scan(ip)
     else:
-        print("ip error!!")
-
-def start():
-    print("hello world!")
-    print("now please waiting ....")
-
-def main():
-    start()
-    ip='47.94.132.67'
-    scan(ip)
+        exit("please input true ip")
 
 if __name__=='__main__':
-    main()
+    main(sys.argv)
